6540b428a0bd02725a6063cbbe065af4ec57af50
mathani-wedo <mathani@dwave-s.com>
2023-10-04 23:35:01 +0300

    update

--- acs_hms_pharmacy/model/hms_base.py
+++ acs_hms_pharmacy/model/hms_base.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import api, fields, models, _
+from odoo.exceptions import UserError
 
 class PrescriptionLine(models.Model):
     _inherit = 'prescription.line'
